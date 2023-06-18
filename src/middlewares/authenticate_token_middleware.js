e0ca665c7dd76bc013438d99ca33142978ac6d79
Shaiane Boesing Rodrigues Borges <shaianeboesingrb@gmail.com>
2023-06-17 21:39:05 -0300

    bug fix

--- src/middlewares/authenticate_token_middleware.js
+++ src/middlewares/authenticate_token_middleware.js
@@ -1,25 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 exports.authenticateToken = function(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-  
-    if (token == null) {
-      return res.status(401).json({ message: 'Token de autenticação não fornecido.' });
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    return res.status(401).json({ message: 'Token de autenticação não fornecido.' });
+  }
+
+  jwt.verify(token, 'sM0n1fW6yHg4aL9zCv2bQp8nRj5uXe3r', (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Falha na autenticação do token.' });
     }
-  
-    jwt.verify(token, 'sM0n1fW6yHg4aL9zCv2bQp8nRj5uXe3r', (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: 'Falha na autenticação do token.' });
-      }
 
-      if (!req.session || !req.session.userId) {
-        return res.status(401).json({ message: 'Usuário não autenticado.' });
-      }
-      req.session.userId = user.id_usuario;
-    });
-
+    if (!req.session || !req.session.userId) {
+      return res.status(401).json({ message: 'Usuário não autenticado.' });
+    }
+    req.session.userId = user.id_usuario;
 
     next();
-  };
-  
+  });
+};
